8cee9e174956c4e400b9b16dfda785fecd0665aa
Timbless <78437748+Timbless@users.noreply.github.com>
2022-08-16 22:00:49 -0400

    Update GoogleImageScraping.py

--- GoogleImageScraping.py
+++ GoogleImageScraping.py
@@ -1,50 +1,38 @@
-import requests
 import os
 import time
-from PIL import Image
-import io
 import urllib.request
-import uuid
-#import bs4
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
-
-
-
-
-#keyword = input("Keyword for search")
-#url = 'https://www.google.com/search?q='+keyword+ '&source=lnms&tbm=isch'
-#url = 'https://www.google.com/search?q=dog&source=lnms&tbm=isch'
 
 def download_pic(url, name, path):
 
     if not os.path.exists(path):
         os.makedirs(path)
     res = urllib.request.urlopen(url, timeout=3).read()
+
     with open(path + name +'.jpg', 'wb') as file:
         file.write(res)
         file.close()
-    #path_1 = os.getcwd()
-    #files = os.listdir(path+name)
 
-    #for index, file in enumerate(files):
-        #os.rename(os.path.join(path_1, file), os.path.join(path_1, ''.join([str(index), '.jpg'])))
+
+#//*[@id="hdtb-msb"]/div[1]/div/div[3]/a
 
 
 def get_image_url(num, key_word):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    url = "https://www.google.com/"
+    #url = "https://www.google.com/" omit this step, because we can directly use google image url
+    
+    #driver.maximize_window() if you want to see what driver does in fullscreen
+    url = 'https://images.google.com/'
     driver.get(url)
     box = driver.find_element('xpath','/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
     #key_word = input('Search for word:\n')
     box.send_keys(key_word)
     box.send_keys(Keys.ENTER)
-    box = driver.find_element('xpath','//*[@id="hdtb-msb"]/div[1]/div/div[2]/a').click()
+    #box = driver.find_element('xpath','//*[@id="hdtb-msb"]/div[1]/div/div[2]/a').click()
 
 
     last_height = driver.execute_script('return document.body.scrollHeight')
@@ -89,19 +77,19 @@ if __name__ == '__main__':
     path = os.getcwd() + '\\'+ key_word + '\\'
     print(path)
     image_urls = get_image_url(num, key_word)
-
+    count = 0
     for index, url in enumerate(image_urls):
         try:
             print('image: '+ str(index) + " start to download")
-            download_pic(url, str(uuid.uuid1()), path)
+            download_pic(url, str(index), path)
+            count += 1
         except Exception as e:
             print(e)
             print('image: '+ str(index) + ' download failure')
             continue
+    print()
+    print(f'Overall, successfully download {count} images')
 
 
 
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-#driver.get(url)
-#driver.maximize_window()
 
